c4dabfeba765ad137fcbc42a837c0b965071c3a9
sorasak menphurk <sorasak_456123@hotmail.com>
2024-05-08 19:06:42 +0700

    change to wherenull deleted_at

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -18,7 +18,7 @@ class CategoryController extends Controller
     {
         DB::beginTransaction();
         try {
-            $categories = CategoryVideo::withTrashed()->get();
+            $categories = CategoryVideo::whereNull('deleted_at')->get();
             DB::commit();
             return response()->json($categories, 200);
         } catch (\Exception $e) {
