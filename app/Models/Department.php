eaa58a1720a66265c51a510bcad9656b8dcd542d
sorasak menphurk <sorasak_456123@hotmail.com>
2024-05-16 17:35:50 +0700

    boot save created_at,updated_at

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -28,4 +28,27 @@ class Department extends Model
     public function getCreatedAtAttribute($value) {
         return \Carbon\Carbon::parse($value)->format('d-m-Y');
     }
+
+    protected static function boot()
+    {
+
+        parent::boot();
+
+        // updating created_by and updated_by when model is created
+        static::creating(function ($model) {
+            if (!$model->isDirty('created_by')) {
+                $model->created_by = auth()->user()->id;
+            }
+            if (!$model->isDirty('updated_by')) {
+                $model->updated_by = auth()->user()->id;
+            }
+        });
+
+        // updating updated_by when model is updated
+        static::updating(function ($model) {
+            if (!$model->isDirty('updated_by')) {
+                $model->updated_by = auth()->user()->id;
+            }
+        });
+    }
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -42,4 +42,27 @@ class Employee extends Model
         return $this->hasMany(EmployeeVideo::class, 'employee_id', 'id');
     }
 
+    protected static function boot()
+    {
+
+        parent::boot();
+
+        // updating created_by and updated_by when model is created
+        static::creating(function ($model) {
+            if (!$model->isDirty('created_by')) {
+                $model->created_by = auth()->user()->id;
+            }
+            if (!$model->isDirty('updated_by')) {
+                $model->updated_by = auth()->user()->id;
+            }
+        });
+
+        // updating updated_by when model is updated
+        static::updating(function ($model) {
+            if (!$model->isDirty('updated_by')) {
+                $model->updated_by = auth()->user()->id;
+            }
+        });
+    }
+
 }
